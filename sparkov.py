4ec6b8d231cbab4a3ea92fe5a3669352b2c14bc4
Baisang <sang.bri@gmail.com>
2015-09-26 00:21:03 -0700

    collectAsMap() but not what is needed

--- sparkov.py
+++ sparkov.py
@@ -2,7 +2,7 @@ from pyspark import SparkContext, SparkConf
 
 appName = 'sparkov'
 master = 'local[8]'
-fileName = ''
+fileName = 'pride.txt'
 
 conf = SparkConf().setAppName(appName).setMaster(master)
 sc = SparkContext(conf=conf);
@@ -10,13 +10,13 @@ sc = SparkContext(conf=conf);
 """word_line is a list of words in the sentence like so:
 ['This', 'is', 'a', 'sentence.']
 We return tuple mappings like so:
-((('This', 'is'), 'a'), 1)
+(('This', 'is'), 'a'))
 in a list
 """
 def create_tuples(word_line):
 	end_list = []
 	for i in range(len(word_line) - 2):
-		tup = (((word_line[i], word_line[i+1]), word_line[i+2]), 1)
+		tup = ((word_line[i], word_line[i+1]), word_line[i+2])
 		end_list.append(tup)
 	return end_list
 
@@ -24,8 +24,16 @@ def create_tuples(word_line):
 dumpFile = sc.textFile(fileName)
 dumpFile = dumpFile.map(lambda s: s.lower())
 line_words = dumpFile.map(lambda s: s.split(" "))
-tuples = line_words.flatMap(create_tuples)
-reduced = tuples.reduceByKey(lambda a, b: a + b)
-reduced.saveAsTextFile(fileName+'.reduced')
+tuples = line_words.flatMap(create_tuples).cache()
+# I don't think this mapping maps K --> many V, so will have to change
+markov_dict = tuples.collectAsMap()
+
+# the following gets the counts of each tuple
+tuple_count = tuples.map(lambda t: (t, 1))
+count = tuple_count.reduceByKey(lambda a, b: a + b).cache()
+
+
+
+count.saveAsTextFile(fileName+'.counts')
 
 
