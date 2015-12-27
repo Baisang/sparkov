dfda1fffdfe944514cc1ba674fa6175152178929
Baisang <sang.bri@gmail.com>
2015-12-26 19:14:52 -0800

    more functions so you can call it

--- sparkov.py
+++ sparkov.py
@@ -1,11 +1,7 @@
 from pyspark import SparkContext, SparkConf
 
-appName = 'sparkov'
-master = 'local[8]'
-fileName = 'pride.txt'
-
-conf = SparkConf().setAppName(appName).setMaster(master)
-sc = SparkContext(conf=conf);
+_fileName = ''
+sc = None
 
 """word_line is a list of words in the sentence like so:
 ['This', 'is', 'a', 'sentence.']
@@ -13,7 +9,7 @@ We return tuple mappings like so:
 (('This', 'is'), 'a'))
 in a list
 """
-def create_tuples(word_line):
+def _create_tuples(word_line):
 	end_list = []
 	for i in range(len(word_line) - 2):
 		tup = ((word_line[i], word_line[i+1]), word_line[i+2])
@@ -21,24 +17,34 @@ def create_tuples(word_line):
 	return end_list
 
 
-dumpFile = sc.textFile(fileName)
-dumpFile = dumpFile.map(lambda s: s.lower())
-line_words = dumpFile.map(lambda s: s.split(" "))
-tuples = line_words.flatMap(create_tuples).cache()
-# map tuple (('This', 'is'), 'a')) to (('This', 'is'), ['a'])
-# so then when adding with another tuple with key ('This', 'is'),
-# can just concat the list value
-tuple_map = tuples.map(lambda t: (t[0], [t[1]]))
-tuple_map = tuple_map.reduceByKey(lambda a, b: a + b)
-# turn into a dictionary!!
-tuple_dict = tuple_map.collectAsMap()
+""" This function must be called first to initialize the SparkContext.
+"""
+def configure(appName, master, fileName):
+	_fileName = fileName
+	conf = SparkConf().setAppName(appName).setMaster(master)
+	sc = SparkContext(conf=conf)
 
-# the following gets the counts of each tuple
-tuple_count = tuples.map(lambda t: (t, 1))
-count = tuple_count.reduceByKey(lambda a, b: a + b).cache()
+"""Creates a dictionary mapping each pair of adjacent words to the following word.
+If the optional parameters writeToFile and destination are specified, will also
+write out to a file the pairings along with the count for each mapping.
+"""
+def create_dictionary(writeToFile=False, destination=None):
+	dumpFile = sc.textFile(_fileName)
+	dumpFile = dumpFile.map(lambda s: s.lower())
+	line_words = dumpFile.map(lambda s: s.split(" "))
+	tuples = line_words.flatMap(_create_tuples).cache()
+	# map tuple (('This', 'is'), 'a')) to (('This', 'is'), ['a'])
+	# so then when adding with another tuple with key ('This', 'is'),
+	# can just concat the list value
+	tuple_map = tuples.map(lambda t: (t[0], [t[1]]))
+	tuple_map = tuple_map.reduceByKey(lambda a, b: a + b)
+	# turn into a dictionary!!
+	tuple_dict = tuple_map.collectAsMap()
 
+	if writeToFile and destination:
+		# the following gets the counts of each tuple
+		tuple_count = tuples.map(lambda t: (t, 1))
+		count = tuple_count.reduceByKey(lambda a, b: a + b).cache()
+		count.saveAsTextFile(destination)
 
-
-count.saveAsTextFile(fileName+'.counts')
-
-
+	return tuple_dict
